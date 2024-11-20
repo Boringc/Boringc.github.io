3d6364992f0a6dd3e6ef2adfa1d6afa044f71e5b
Boringc <1344549903@qq.com>
2024-11-20 15:08:17 +0800

    Site updated: 2024-11-20 15:08:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/20/hello-world/","2024/11/20/world/","2024/11/20/测试博文/"];function toRandomPost(){
+var posts=["2024/11/20/测试博文/","2024/11/20/world/","2024/11/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
